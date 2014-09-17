f3a1f951088a1012b0bb4940a17c30cc228b7e7f
Qianqian Jiao <qiqjiao@gmail.com>
2014-09-17 13:15:39 -0700

    Study Nginx

---
+++ nginx/install/request.sh
@@ -0,0 +1,16 @@
+curl http://127.0.0.1:8080/search/ &
+curl http://127.0.0.1:8080/search/ &
+curl http://127.0.0.1:8080/search/ &
+curl http://127.0.0.1:8080/search/ &
+curl http://127.0.0.1:8080/search/ &
+curl http://127.0.0.1:8080/search/ &
+
+curl http://127.0.0.1:8080/search2/ &
+curl http://127.0.0.1:8080/search2/ &
+curl http://127.0.0.1:8080/search2/ &
+curl http://127.0.0.1:8080/search2/ &
+curl http://127.0.0.1:8080/search2/ &
+curl http://127.0.0.1:8080/search2/ &
+
+curl -v http://127.0.0.1:8080/search/ 
+curl -v  http://127.0.0.1:8080/search2/
