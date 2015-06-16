1f9c053259014f5fd27f031f45b7ce35cc2f22e4
Qianqian Jiao <qiqjiao@gmail.com>
2015-06-16 12:50:12 -0700

    sync

--- cpp/asio.yield.cpp
+++ cpp/asio.yield.cpp
@@ -1,45 +1,81 @@
 #include <boost/asio.hpp>
+#include <boost/asio/spawn.hpp>
+#include <memory>
+#include <functional>
+#include <iostream>
 
 
-void session(boost::asio::io_service& io_service){
-    // construct TCP-socket from io_service
-    boost::asio::ip::tcp::socket socket(io_service);
+class session : public std::enable_shared_from_this < session >
+{
+public:
+    explicit session(boost::asio::io_service& io_service)
+        : strand_(io_service), socket_(io_service)
+    {
+    }
 
-    try{
-        for(;;){
-            // local data-buffer
-            char data[max_length];
+    boost::asio::ip::tcp::socket& socket()
+    {
+        return socket_;
+    }
 
-            boost::system::error_code ec;
+    void go()
+    {
+        boost::asio::spawn(strand_,
+            std::bind(&session::echo,
+            shared_from_this(), std::placeholders::_1));
+    }
 
-            // read asynchronous data from socket
-            // execution context will be suspended until
-            // some bytes are read from socket
-            std::size_t length=socket.async_read_some(
-                    boost::asio::buffer(data),
-                    boost::asio::yield[ec]);
-            if (ec==boost::asio::error::eof)
-                break; //connection closed cleanly by peer
-            else if(ec)
-                throw boost::system::system_error(ec); //some other error
-
-            // write some bytes asynchronously
-            boost::asio::async_write(
-                    socket,
-                    boost::asio::buffer(data,length),
-                    boost::asio::yield[ec]);
-            if (ec==boost::asio::error::eof)
-                break; //connection closed cleanly by peer
-            else if(ec)
-                throw boost::system::system_error(ec); //some other error
+private:
+    void echo(boost::asio::yield_context yield)
+    {
+        char data[128];
+        for (;;)
+        {
+            std::size_t n = socket_.async_read_some(boost::asio::buffer(data), yield);
+            boost::asio::async_write(socket_, boost::asio::buffer(data, n), yield);
         }
-    } catch(std::exception const& e){
-        std::cerr<<"Exception: "<<e.what()<<"\n";
+    }
+
+    boost::asio::io_service::strand strand_;
+    boost::asio::ip::tcp::socket socket_;
+};
+
+void do_accept(boost::asio::io_service& io_service,
+    unsigned short port, boost::asio::yield_context yield)
+{
+    boost::asio::ip::tcp::acceptor acceptor(
+        io_service,
+        boost::asio::ip::tcp::endpoint(
+        boost::asio::ip::tcp::v4(), port));
+
+    for (;;)
+    {
+        boost::system::error_code ec;
+        std::shared_ptr<session> new_session(std::make_shared<session>(io_service));
+        acceptor.async_accept(new_session->socket(), yield[ec]);
+        if (!ec)
+        {
+            new_session->go();
+        }
     }
 }
 
-int main() {
-  boost::asio::io_service io_service;
-  session(io_service);
-  return 0;
+int main(int argc, char* argv[])
+{
+    try
+    {
+        boost::asio::io_service io_service;
+
+        boost::asio::spawn(io_service,
+            std::bind(do_accept,
+            std::ref(io_service), 8888, std::placeholders::_1));
+
+        io_service.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
 }
